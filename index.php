81893674c5370e45366a6b42ebc3ac4dff6cda2b
sergey ruskov <sergeyruskov.31@gmail.com>
2019-12-20 22:03:37 +0200

    he

--- index.php
+++ index.php
@@ -1,25 +1,24 @@
-<?php 
+<?php
 
 $service_name = @$_POST['service'];
 header('Access-Control-Allow-Methods: POST, GET');
 header('Access-Control-Allow-Headers: *');
-if($service_name != ''){
-	if(function_exists(@service_name)){
-		$service_name();
-	}else{
-		echo false;
-	}
+if ($service_name != '') {
+    if (function_exists($service_name)) {
+        $service_name();
+    } else
+        echo false;
 }
 
-function money(){
-	$result = false;
-	//$result = '3.007.000,00';
-	echo json_encode($result)
+function money()
+{
+    $result = false;
+    //$result = '3.300.000,00';
+    echo json_encode($result);
 }
 
-function crash(){
-	$result = false;
-	echo json_encode($result);
+function crash()
+{
+    $result = true;
+    echo json_encode($result);
 }
-
-?>
