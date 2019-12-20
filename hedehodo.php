559efeaa5e81ba75225400850abac52dd1f344a2
sergey ruskov <sergeyruskov.31@gmail.com>
2019-12-20 17:18:30 +0200

    make it better

---
+++ hedehodo.php
@@ -0,0 +1,25 @@
+<?php 
+
+$service_name = @$_POST['service'];
+header('Access-Control-Allow-Methods: POST, GET');
+header('Access-Control-Allow-Headers: *');
+if($service_name != ''){
+	if(function_exists(@service_name)){
+		$service_name();
+	}else{
+		echo false;
+	}
+}
+
+function money(){
+	$result = false;
+	//$result = '3.007.000,00';
+	echo json_encode($result)
+}
+
+function crash(){
+	$result = false;
+	echo json_encode($result);
+}
+
+?>
