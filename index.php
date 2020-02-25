6279aacf2448d9abc05177eb1674c3be58f6a942
sergey ruskov <sergeyruskov.31@gmail.com>
2020-02-25 14:03:57 +0200

    sad

--- index.php
+++ index.php
@@ -12,8 +12,8 @@ if ($service_name != '') {
 
 function money()
 {
-    //$result = false;
-    $result = '3.300.000,00';
+    $result = false;
+    //$result = '3.300.000,00';
     echo json_encode($result);
 }
 
