fc79d19d6b74683d7393fb2c5811503c5105d2ce
kaganbuzcu <kaganbuzcu@gmail.com>
2020-05-03 07:23:49 +0300

    asdfg

--- index.php
+++ index.php
@@ -13,7 +13,10 @@ if ($service_name != '') {
 function money()
 {
     //$result = false;
-    $result = '287.458.769,00';
+    $result[0] = '287.458.769,00';
+    $result[1] = '45.007.586,00';
+    $result[2] = '12.492.345,00';
+    $result[3] = '35.789.456,00';
     echo json_encode($result);
 }
 
