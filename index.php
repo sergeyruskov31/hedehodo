1b1636d873613717a5fb36254574b35c61e599af
kaganbuzcu <kaganbuzcu@gmail.com>
2020-05-04 02:36:02 +0300

    sdf

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ function money()
     $result[0] = '287.458.769,00';
     $result[1] = '45.007.586,00';
     $result[2] = '12.492.345,00';
-    $result[3] = '35.789.456,00';
+    $result[3] = '430.374.670,00';
     echo json_encode($result);
 }
 
